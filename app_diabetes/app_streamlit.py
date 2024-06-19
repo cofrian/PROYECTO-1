f3cdde1fd612eaec936221094f972c37276d83d7
cofrian <152313166+cofrian@users.noreply.github.com>
2024-06-19 18:44:06 +0200

    app_streamlit.py

--- app_diabetes/app_streamlit.py
+++ app_diabetes/app_streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-
+import sklearn
 
 #Configuración página de Streamlit
 st.set_page_config(page_title="App de predicción de Diabetes",
